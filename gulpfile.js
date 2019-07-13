00784a6978ba3b20a57e147cc892bcdc2994b172
Alberto Baños <abanod00@estudiantes.unileon.es>
2019-07-14 00:03:35 +0200

    Fix name

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,26 @@ var gulp = require('gulp'),
   webserver = require('gulp-webserver');
 
 gulp.task('js', function() {
-  gulp.src('builds/CMSAcademy19/js/**/*');
+  gulp.src('builds/CDSAcademy19/js/**/*');
 });
 
 gulp.task('html', function() {
-  gulp.src('builds/CMSAcademy19/*.html');
+  gulp.src('builds/CDSAcademy19/*.html');
 });
 
 gulp.task('css', function() {
-  gulp.src('builds/CMSAcademy19/css/*.css');
+  gulp.src('builds/CDSAcademy19/css/*.css');
 });
 
 gulp.task('watch', function() {
-  gulp.watch('builds/CMSAcademy19/js/**/*', ['js']);
-  gulp.watch('builds/CMSAcademy19/css/*.css', ['css']);
-  gulp.watch(['builds/CMSAcademy19/*.html',
-    'builds/CMSAcademy19/views/*.html'], ['html']);
+  gulp.watch('builds/CDSAcademy19/js/**/*', ['js']);
+  gulp.watch('builds/CDSAcademy19/css/*.css', ['css']);
+  gulp.watch(['builds/CDSAcademy19/*.html',
+    'builds/CDSAcademy19/views/*.html'], ['html']);
 });
 
 gulp.task('webserver', function() {
-  gulp.src('builds/CMSAcademy19/')
+  gulp.src('builds/CDSAcademy19/')
     .pipe(webserver({
       livereload: true,
       open: true
